02d251c578765b2d06d3ea7dd8859a58865e6ca3
Mansi Gupta <miamortinta@outlook.com>
2022-04-07 15:45:02 +0530

    Second Commit

--- app/src/main/java/com/example/mymemory/MemoryBoardAdaptor.kt
+++ app/src/main/java/com/example/mymemory/MemoryBoardAdaptor.kt
@@ -1,16 +1,29 @@
 package com.example.mymemory
-
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+
+
 
 class MemoryBoardAdaptor(private val context: Context, private val numPieces: Int) :
     RecyclerView.Adapter<MemoryBoardAdaptor.ViewHolder>() {
 
+    companion object {
+        private const val MARGIN_SIZE = 10
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val cardWidth = parent.width / 2 - (2 * MARGIN_SIZE)
+        val cardHeight = parent.height / 4 - (2 * MARGIN_SIZE)
+        val cardSideLength = kotlin.math.min(cardWidth, cardHeight)
         val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
         return ViewHolder(view)
 
 
